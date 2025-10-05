1ecfa75d0897a442b09c28adc617aa8abb0a3a85
Shaji-Mohammed <mshajisufiyan@gmail.com>
2025-10-04 22:24:26 -0300

    fix pointer bug for tech stack

--- src/components/Models/TechLogos/TechIcon.tsx
+++ src/components/Models/TechLogos/TechIcon.tsx
@@ -2,19 +2,26 @@ import { Environment, Float, OrbitControls, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import type { GLTF } from "three-stdlib";
 // @ts-ignore
-import {Aws_logo} from './Aws_logo';
+import { Aws_logo } from "./Aws_logo";
+import { useMediaQuery } from "react-responsive";
 
-const TechIcon = ({ model } : any) => {
+const TechIcon = ({ model }: any) => {
+  const isMobile = useMediaQuery({ query: "(max-width: 1024px)" });
+
   if (model.name === "AWS") {
     return (
-      <Canvas>
+      <Canvas style={{ pointerEvents: isMobile ? "none" : "auto" }}>
         <ambientLight intensity={1} />
         <directionalLight position={[5, 5, 5]} intensity={4} />
         <Environment preset="night" />
         <OrbitControls enableZoom={false} />
 
         <Float speed={5.5} rotationIntensity={0.5} floatIntensity={0.9}>
-          <group scale={model.scale} position={model.position || [0, 0, 0]} rotation={model.rotation}>
+          <group
+            scale={model.scale}
+            position={model.position || [0, 0, 0]}
+            rotation={model.rotation}
+          >
             <Aws_logo />
           </group>
         </Float>
@@ -25,7 +32,7 @@ const TechIcon = ({ model } : any) => {
   const scene = useGLTF(model.modelPath) as GLTF;
 
   return (
-    <Canvas>
+    <Canvas style={{ pointerEvents: isMobile ? "none" : "auto" }}>
       <ambientLight intensity={0.5} />
 
       <directionalLight position={[5, 5, 5]} intensity={1.5} />
